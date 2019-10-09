bb5e8e78a02774a9b13991d0dc465688f96caed4
Alexander Borodich <sasha.barodach@gmail.com>
2019-10-09 21:30:02 +0300

    feat: implement GuessingGame task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+			this.min = min;
+			this.max = max;
     }
 
     guess() {
-
+			this.newRange = this.max - this.min;
+			this.number = Math.ceil(this.newRange / 2) + this.min;
+			return this.number;
     }
 
     lower() {
-
+			this.max = this.number;
     }
 
     greater() {
-
+			this.min = this.number;
     }
 }
 
